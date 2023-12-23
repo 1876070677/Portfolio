2ac404104592d31bf0fdb8ca22b06c9a52024d90
KSH <1876070677@shbox.shop>
2023-12-24 08:54:22 +0900

    link.js

--- frontend/src/static/link.js
+++ frontend/src/static/link.js
@@ -7,8 +7,8 @@
 // export const GET_USER_INFO = "https://api.shbox.kr/userinfo";
 // export const REFRESH_TOKEN = "https://api.shbox.kr/refresh";
 // export const USER_LOGIN = "https://api.shbox.kr/user/login";
-export const GET_ACTIVITY = "http://portfolioAPI:5557/act"
-export const GET_PORTFOLIO = "http://portfolioAPI:5557/portfolio"
+// export const GET_ACTIVITY = "http://portfolioAPI:5557/act"
+// export const GET_PORTFOLIO = "http://portfolioAPI:5557/portfolio"
 export const GET_ACTIVITY = "https://api.shbox.kr/act"
 export const GET_PORTFOLIO = "https://api.shbox.kr/portfolio"
 
