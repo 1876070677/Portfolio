68da917b66f327abe3a1608322e765a9fca48378
KSH <1876070677@shbox.shop>
2023-12-24 08:52:01 +0900

    external access

--- frontend/src/static/link.js
+++ frontend/src/static/link.js
@@ -9,6 +9,8 @@
 // export const USER_LOGIN = "https://api.shbox.kr/user/login";
 export const GET_ACTIVITY = "http://portfolioAPI:5557/act"
 export const GET_PORTFOLIO = "http://portfolioAPI:5557/portfolio"
+export const GET_ACTIVITY = "https://api.shbox.kr/act"
+export const GET_PORTFOLIO = "https://api.shbox.kr/portfolio"
 
 // export const GET_USER_INFO = "api:3007/userinfo";
 // export const REFRESH_TOKEN = "api:3007/refresh";
