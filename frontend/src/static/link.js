eeea21063e30ccfa5d1f49485df6140ab84743ed
KSH <1876070677@shbox.shop>
2023-12-25 01:04:41 +0900

    frontend

--- frontend/src/static/link.js
+++ frontend/src/static/link.js
@@ -11,6 +11,8 @@
 // export const GET_PORTFOLIO = "http://portfolioAPI:5557/portfolio"
 export const GET_ACTIVITY = "https://api.shbox.kr/act"
 export const GET_PORTFOLIO = "https://api.shbox.kr/portfolio"
+// export const GET_ACTIVITY = "http://localhost:5557/act"
+// export const GET_PORTFOLIO = "http://localhost:5557/portfolio"
 
 // export const GET_USER_INFO = "api:3007/userinfo";
 // export const REFRESH_TOKEN = "api:3007/refresh";
